83e787e92955d0eb3c92def927e9af49e579b928
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2017-10-29 16:32:11 +0100

    Cartesian production for Burgers' test

--- burgers/test_burgers.py
+++ burgers/test_burgers.py
@@ -6,6 +6,7 @@ Check that numerical error of the Godunov solver with various kernel versions
 
 from burgers import main, KERNELS
 from pytest import mark, approx
+import itertools
 import sys
 
 if sys.platform == "darwin":
@@ -14,18 +15,12 @@ if sys.platform == "darwin":
     KERNELS.remove("pythran")
 
 
-@mark.parametrize('nmax', [100, 1000])
-def test_nmax(nmax):
+@mark.parametrize('nmax, kernel', itertools.product([100, 1000], KERNELS))
+def test_nmax_kernel(nmax, kernel):
+    """Test a combination of nmax values and kernel versions and compare to
+    expected numerical error"""
     tmax = 1.0
     err_ref = {100: 0.04496958454369648,
                1000: 0.018877105158683641}
-    error = main(tmax, nmax, kernel='python')
-    assert error == approx(err_ref[nmax])
-
-
-@mark.parametrize('kernel', KERNELS)
-def test_kernels(kernel):
-    tmax = 1.0
-    nmax = 100
     error = main(tmax, nmax, kernel=kernel)
-    assert error == approx(0.04496958454369648)
+    assert error == approx(err_ref[nmax])
