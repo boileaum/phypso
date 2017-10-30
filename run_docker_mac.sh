2d493589eb743d01ca7cbe0934af0dc69b48e2b6
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2017-10-30 15:14:43 +0100

    run_docker_mac.sh: better handle network interface and exiting

--- run_docker_mac.sh
+++ run_docker_mac.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+trap 'kill $BGPID; exit' SIGINT
 socat TCP-LISTEN:6001,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+BGPID=$!
+active_interface_name=$(ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active' | egrep -o -m 1 '^[^\t:]+')
 docker run -ti --rm  -e XAUTHORITY=/tmp/xauth \
        -v ~/.Xauthority:/tmp/xauth \
-       -e DISPLAY=$(ifconfig en0 | grep 'inet '|awk '{print $2}'):1 \
+       -e DISPLAY=$(ifconfig $active_interface_name | grep 'inet '|awk '{print $2}'):1 \
        --net host \
        -v $(pwd):/home/euler/phypso boileaum/phypso-env
-
+kill $BGPID; exit
