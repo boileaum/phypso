09edc6efe2af805f747f72759a0779dd3d2e6a3f
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2017-11-12 21:15:12 +0100

    Rewrite burgers/godunov_numpy.py

--- burgers/godunov_numpy.py
+++ burgers/godunov_numpy.py
@@ -10,6 +10,22 @@ from godunov import xmin, xmax, sol_exact
 import numpy as np
 
 
+def riemann_burgers(wL, wR, xi):
+    """Exact Riemann solver for Burgers' equation"""
+    sigma = (wL + wR)/2.
+    w = (wL < wR)*((xi < wL)*wL +
+                   (xi > wR)*wR + ((xi >= wL) & (xi <= wR))*xi) + \
+        (wL >= wR)*((xi < sigma)*wL + (xi >= sigma)*wR)
+    return w
+
+
+def numflux(wL, wR):
+    """Return numerical flux using Riemann solver"""
+    w = riemann_burgers(wL, wR, 0.)
+    flux = w*w/2.
+    return flux
+
+
 def timeloop(tmax, nmax):
     """Iterate overt time to return the solution at t = tmax"""
     dx = float(xmax - xmin)/nmax
@@ -23,19 +39,9 @@ def timeloop(tmax, nmax):
         vmax = max(abs(wn))
         dt = cfl*dx/vmax
 
-        wl = wn[:-1]
-        wr = wn[1:]
+        flux = numflux(wn[:-1], wn[1:])
+        wn[1:-1] -= dt/dx*(flux[1:] - flux[:-1])
 
-        sigma = 0.5*(wl + wr)
-        w_shock = wl if np.where(sigma > 0.) else wr
-
-        condlist = [wl >= 0., wr <= 0., (wl < 0) * (wr > 0)]
-        choicelist = [wl, wr, 0.]
-        w_expan = np.select(condlist, choicelist)
-
-        w = w_shock if np.where(wl > wr) else w_expan
-
-        wn[1:-1] = wn[1:-1] - 0.5*dt/dx*w[1:]**2 + 0.5*dt/dx*w[:-1]**2
         t += dt
 
     return xm, wn
